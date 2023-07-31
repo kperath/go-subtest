eb325fc71a822e4e2b054fdd5b9274155d419471
Kunju Perath <kperath@gmail.com>
2023-07-30 23:45:30 -0400

    reformat

--- src/extension.ts
+++ src/extension.ts
@@ -31,140 +31,134 @@ export function activate(context: vscode.ExtensionContext) {
       const editor = vscode.window.activeTextEditor;
       if (!vscode.workspace.workspaceFolders) {
         vscode.window.showErrorMessage(
-          "extension must be ran in a vscode workspace"
+          "Extension must be ran in a vscode workspace"
         );
         return;
       }
       const path = vscode.workspace.workspaceFolders[0].uri.path;
 
-      if (editor) {
-        // get current cursor line
-        const cursorPosNum = editor.document.offsetAt(editor.selection.active);
-        const cursorPos = editor.document.positionAt(cursorPosNum);
-        // get all words surround in quotes (the subtest name)
-        const line = editor.document.lineAt(cursorPos);
-        if (line.isEmptyOrWhitespace) {
-          return;
-        }
-        const match = line.text.match(/".*"/);
-        if (!match) {
-          return;
-        }
-        const subTestName = match[0].replaceAll('"', "").replaceAll(" ", "_");
+      if (!editor) {
+        vscode.window.showErrorMessage("Editor window not open");
+        return;
+      }
+      // get current cursor line
+      const cursorPosNum = editor.document.offsetAt(editor.selection.active);
+      const cursorPos = editor.document.positionAt(cursorPosNum);
+      // get all words surround in quotes (the subtest name)
+      const line = editor.document.lineAt(cursorPos);
+      if (line.isEmptyOrWhitespace) {
+        return;
+      }
+      const match = line.text.match(/".*"/);
+      if (!match) {
+        return;
+      }
+      const subTestName = match[0].replaceAll('"', "").replaceAll(" ", "_");
 
-        // list all symbols in the document
-        vscode.commands
-          .executeCommand<vscode.DocumentSymbol[]>(
-            "vscode.executeDocumentSymbolProvider",
-            editor.document.uri
-          )
-          .then((symbols) => {
-            for (const symbol of symbols) {
-              const testName = symbol.name;
-              if (!testName.startsWith("Test")) {
+      // list all symbols in the document
+      vscode.commands
+        .executeCommand<vscode.DocumentSymbol[]>(
+          "vscode.executeDocumentSymbolProvider",
+          editor.document.uri
+        )
+        .then((symbols) => {
+          for (const symbol of symbols) {
+            const testName = symbol.name;
+            if (!testName.startsWith("Test")) {
+              vscode.window.showErrorMessage("This is not a go test function");
+              return;
+            }
+            // get current symbol cursor is focused on
+            if (symbol.range.contains(cursorPos)) {
+              const currentSubTest = testName + "/" + subTestName;
+
+              // find build tags
+              let buildTags = "";
+              const documentText = editor.document.getText();
+              const match = documentText.match("//go:build");
+              if (match) {
+                let matchIndex = 0;
+                if (match.index) {
+                  matchIndex = match.index;
+                }
+                const matchPos = editor.document.positionAt(matchIndex);
+                const line = editor.document.lineAt(matchPos);
+                buildTags = line.text.replace("//go:build", "");
+              }
+
+              const json = generateSubTestJSON(currentSubTest, path, buildTags);
+              // write generated json to launch.json
+              if (!vscode.workspace.workspaceFolders) {
                 vscode.window.showErrorMessage(
-                  "this is not a go test function"
+                  "Extension must be ran in a vscode workspace"
                 );
                 return;
               }
-              // get current symbol cursor is focused on
-              if (symbol.range.contains(cursorPos)) {
-                const currentSubTest = testName + "/" + subTestName;
 
-                // find build tags
-                let buildTags = "";
-                const documentText = editor.document.getText();
-                const match = documentText.match("//go:build");
-                if (match) {
-                  let matchIndex = 0;
-                  if (match.index) {
-                    matchIndex = match.index;
-                  }
-                  const matchPos = editor.document.positionAt(matchIndex);
-                  const line = editor.document.lineAt(matchPos);
-                  buildTags = line.text.replace("//go:build", "");
-                }
+              const launch = vscode.workspace.getConfiguration(
+                "launch",
+                vscode.workspace.workspaceFolders[0].uri
+              );
+              const config = launch.get("configurations");
+              if (config instanceof Array) {
+                // check if config already exists
+                for (let i = 0; i < config.length; i++) {
+                  if (config[i].name == currentSubTest) {
+                    vscode.window
+                      .showWarningMessage(
+                        "launch.json config already exists",
+                        "Open launch.json"
+                      )
+                      .then(() => {
+                        if (!vscode.workspace.workspaceFolders) {
+                          vscode.window.showErrorMessage(
+                            "Extension must be ran in a vscode workspace"
+                          );
+                          return;
+                        }
 
-                const json = generateSubTestJSON(
-                  currentSubTest,
-                  path,
-                  buildTags
-                );
-                // write generated json to launch.json
-                if (!vscode.workspace.workspaceFolders) {
-                  vscode.window.showErrorMessage(
-                    "extension must be ran in a vscode workspace"
-                  );
-                  return;
-                }
-
-                const launch = vscode.workspace.getConfiguration(
-                  "launch",
-                  vscode.workspace.workspaceFolders[0].uri
-                );
-                const config = launch.get("configurations");
-                if (config instanceof Array) {
-                  // check if config already exists
-                  for (let i = 0; i < config.length; i++) {
-                    if (config[i].name == currentSubTest) {
-                      vscode.window
-                        .showWarningMessage(
-                          "launch.json config already exists",
-                          "open launch.json"
-                        )
-                        .then(() => {
-                          if (!vscode.workspace.workspaceFolders) {
-                            vscode.window.showErrorMessage(
-                              "extension must be ran in a vscode workspace"
-                            );
-                            return;
-                          }
-
-                          vscode.workspace
-                            .openTextDocument(path + "/.vscode/launch.json")
-                            .then((launch) => {
-                              vscode.window.showTextDocument(launch);
-                            });
-                        });
-                      return;
-                    }
-                  }
-                  config.push(json);
-                  launch.update("configurations", config);
-                } else {
-                  vscode.window.showErrorMessage(
-                    "configuration field in launch.json should be an array"
-                  );
-                  return;
-                }
-
-                // TODO(kperath): see README
-                // vscode.env.clipboard.writeText(json).then(() => {
-                // });
-                vscode.window
-                  .showInformationMessage(
-                    `copied ${currentSubTest} JSON. Add it to your launch.json`,
-                    "open launch.json"
-                  )
-                  .then(() => {
-                    if (!vscode.workspace.workspaceFolders) {
-                      vscode.window.showErrorMessage(
-                        "extension must be ran in a vscode workspace"
-                      );
-                      return;
-                    }
-                    vscode.workspace
-                      .openTextDocument(path + "/.vscode/launch.json")
-                      .then((launch) => {
-                        vscode.window.showTextDocument(launch);
+                        vscode.workspace
+                          .openTextDocument(path + "/.vscode/launch.json")
+                          .then((launch) => {
+                            vscode.window.showTextDocument(launch);
+                          });
                       });
-                  });
+                    return;
+                  }
+                }
+                config.push(json);
+                launch.update("configurations", config);
+              } else {
+                vscode.window.showErrorMessage(
+                  "Configuration field in launch.json should be an array"
+                );
+                return;
               }
+
+              // TODO(kperath): see README
+              // vscode.env.clipboard.writeText(json).then(() => {
+              // });
+              vscode.window
+                .showInformationMessage(
+                  `Added ${currentSubTest} launch.json`,
+                  "Open launch.json"
+                )
+                .then(() => {
+                  if (!vscode.workspace.workspaceFolders) {
+                    vscode.window.showErrorMessage(
+                      "Extension must be ran in a vscode workspace"
+                    );
+                    return;
+                  }
+                  vscode.workspace
+                    .openTextDocument(path + "/.vscode/launch.json")
+                    .then((launch) => {
+                      vscode.window.showTextDocument(launch);
+                    });
+                });
             }
-          });
-      } else {
-        vscode.window.showErrorMessage("cursor needs to be on a word");
-      }
+          }
+        });
     }
   );
 
